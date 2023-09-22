5c8133ddcd27ecc2f5402d54cc73e959e137d452
naufalharitsprasetia <naufalharisprasetia@gmail.com>
2023-09-22 15:34:26 +0700

    [noris] -> about us

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,45 @@
 @extends('template.main')
 
 @section('contain')
-    <h1 class="text-center mt-4">Halaman About</h1>
+    <h1 class="text-center mt-4">Tentang Kami</h1>
+    <!-- Apa itu Bengkel Start -->
+    <div class="row desc-container p-5 mb-4 bg-body-tertiary rounded-3">
+        <div class="order-2 py-5 col-12 col-lg-7">
+            <h1 class="display-5 fw-bold mt-5">Bengkel TI </h1>
+            <p class="col-md-8">
+                Bengkel TI adalah salah satu unit usaha program studi teknik
+                informatika Universitas Darussalam Gontor yang mengatasi kerusakan
+                atau kendala pada pengguna Device (Laptop,Handphone) LCD, Instalasi
+                software dll, Dan juga Bengkel TI menjual beberapa Aksesoris seperti
+                (Mouse, keyboard, headset, dll).
+            </p>
+            <a href="#">
+                <button class="btn btn-primary btn-lg" type="button">Detail</button>
+            </a>
+        </div>
+        <div class="order-1 desc-image col-12 col-lg-5">
+            <img src="/img/desc-img.png" alt="" class="img-fluid" />
+        </div>
+    </div>
+    <!-- End -->
+    <!-- Apa itu Bengkel Start -->
+    <div class="row desc-container p-5 mb-4 bg-body-tertiary rounded-3">
+        <div class="order-2 py-5 col-12 col-lg-7">
+            <h1 class="display-5 fw-bold mt-5">Bengkel TI </h1>
+            <p class="col-md-8">
+                Bengkel TI adalah salah satu unit usaha program studi teknik
+                informatika Universitas Darussalam Gontor yang mengatasi kerusakan
+                atau kendala pada pengguna Device (Laptop,Handphone) LCD, Instalasi
+                software dll, Dan juga Bengkel TI menjual beberapa Aksesoris seperti
+                (Mouse, keyboard, headset, dll).
+            </p>
+            <a href="#">
+                <button class="btn btn-primary btn-lg" type="button">Detail</button>
+            </a>
+        </div>
+        <div class="order-1 desc-image col-12 col-lg-5">
+            <img src="/img/desc-img.png" alt="" class="img-fluid" />
+        </div>
+    </div>
+    <!-- End -->
 @endsection
